3c90aa2e5afc420c54b46be7859ef1c76fe2676b
manly-hen <amhuhn@livin-the-dreem.com>
2013-09-27 07:03:43 -0400

    Initial commit; creating new solution/projects.

---
+++ ConnectGadget/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ConnectGadget.Global" Language="C#" %>
---
+++ DataPortal/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataPortal
+{
+    public class Class1
+    {
+    }
+}
